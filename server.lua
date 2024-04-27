4d4650788b30b8efd961051e1144dd0a30f11f35
KuroNeko <20273059+imkuroneko@users.noreply.github.com>
2024-04-27 02:03:04 -0400

    Update server.lua

--- server.lua
+++ server.lua
@@ -15,6 +15,19 @@ lib.callback.register('neko_radio:server:verify_has_item', function(source, cb)
     end
 end)
 
+lib.callback.register('neko_radio:server:wet_radio', function(source, cb)
+    if Config.requireItem and Config.shouldBrokenRadioOnWater then
+        for key, itemName in pairs(Config.itemName) do
+            local items = ox_inventory:Search(source, 'count', { itemName })
+            if items and items > 0 then
+                ox_inventory:RemoveItem(source, itemName, items)
+                ox_inventory:AddItem(source, itemName..'_wet', items)
+            end
+        end
+    end
+end)
+
+
 if Config.requireItem then
     for key, item in pairs(Config.itemName) do
         CreateItem(item)
